bffbf223dca6057191231ac6ebc136f06e2eb295
shub1709 <shubhamnits@gmail.com>
2025-07-02 13:40:45 +0530

    Added additional domain to 'Allowed Hosts'

--- project/settings.py
+++ project/settings.py
@@ -8,7 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-production')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'app-search-eh8o.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
