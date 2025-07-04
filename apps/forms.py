7f6d3dcadcb0acfa73ae4542c7696d0f4ecf8c3e
shub1709 <shubhamnits@gmail.com>
2025-07-04 08:52:47 +0530

    Added this class to throw the error while registering a new account which already exists

--- apps/forms.py
+++ apps/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import UserReview
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ReviewForm(forms.ModelForm):
     class Meta:
@@ -15,3 +17,20 @@ class ReviewForm(forms.ModelForm):
                 'class': 'form-control'
             })
         }
+
+# Added this class to throw the error while registering a new account which already exists
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        # Remove "password2 mismatch" error if username error exists
+        if 'username' in self.errors and 'password2' in self.errors:
+            password2_errors = self.errors.get('password2')
+            if password2_errors and "didn't match" in password2_errors[0]:
+                del self.errors['password2']
+
+        return cleaned_data
